883d27e75e0fe4896e8fc930c9a961e287222d64
Tasokun <tasokun@gmail.com>
2018-02-23 10:30:59 -0700

    Added new updates

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,18 @@ router.get("/", function(req, res){
 	res.render("index");
 });
 
+router.get("/projects", function(req,res){
+	res.render("projects");
+});
+
+router.get("/aboutme", function(req,res){
+	res.render("aboutme");
+});
+
+router.get("/contactme", function(req,res){
+	res.render("contactme");
+})
+
 router.get("/projects/:projectID", function(req,res){
 	res.render("project");
 });
